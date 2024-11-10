fc308ac137b9a6adc83b89466c775ab1d95bfcab
Griffin Adelmann <griffinadelmann@gmail.com>
2024-11-10 15:05:08 -0500

    mobile nav

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const projects = document.querySelector(".main").children
 for (let proj of projects) {
     proj.children[1].addEventListener("click", () => {
         if (proj.children[3].style.display != "block") {
-            proj.children[1].style.rotate = "-135deg"
+            proj.children[1].style.rotate = "45deg"
             proj.children[3].style.display = "block"
             proj.children[2].style.display = "none"
 
